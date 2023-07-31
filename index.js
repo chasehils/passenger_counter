a04912829fd24d880a3a54991bd0666aa3966277
usernamechasehils <chasehilsinger@gmail.com>
2023-07-31 11:22:07 -0400

    2nd

---
+++ index.js
@@ -0,0 +1,22 @@
+// document.getElementById("count-el").innerText = 5
+
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+
+
+// let count = 5 
+// count = count + 1 
+// console.log(count)
+let count = 0 
+
+function increment() {
+  console.log(' button was clicked')
+  count = count + 1
+  console.log(count)
+}
+
+function dugg() {
+  console.log(42)
+}
+dugg()
